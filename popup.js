4ad86a01016bd4044518d4ef634eed605c673438
km5762 <kumyles@gmail.com>
2024-01-13 11:11:52 -0500

    FWEJIEWIFWE

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 document
   .getElementById("download-button")
   .addEventListener("click", function () {
-    chrome.runtime.sendMessage({ action: "downloadButtonClicked" });
+    chrome.runtime.sendMessage({ action: "DOWNLOAD_BUTTON_CLICKED" });
   });
+
+//  if on course page
+//    download files from
+//    Files, Modules, Discussions, Announcements
+
+// if on dashboard
+//  enumerate all courses
+//    for each course, do whats on course page
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,38 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "canvas.wpi.edu", pathPrefix: "/courses/" },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+      },
+    ]);
+  });
+});
+
 chrome.runtime.onMessage.addListener((message) => {
-  console.log(message);
-  if (message.action === "downloadButtonClicked") {
-    console.log("YEEEYEEEYEEE");
+  if (message.action === "DOWNLOAD_BUTTON_CLICKED") {
+    console.log(message);
+    downloadFile("https://canvas.wpi.edu/api/v1/files/6230398");
   }
 });
+
+async function downloadFile(url) {
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  chrome.downloads.download({
+    url: responseJson.url,
+    filename: responseJson.filename, //Optional
+  });
+}
+
+// Modules
+// if url matches `hostname`/courses/<course_id>/*
+//  let files = []
+//  let course_id = course_id
+//  let modules = GET `hostname`/api/v1/courses/<course_id>/modules :json
+//  files.append(modules.for_each(get_files_from_module_entry))
