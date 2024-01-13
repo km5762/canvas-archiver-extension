c2b0cb7819bc0dd14b974aea939152ee537c09b9
km5762 <kumyles@gmail.com>
2024-01-13 11:58:58 -0500

    ITS LIT

--- course/course.js
+++
@@ -1,2 +0,0 @@
-const url = window.local.hostname
-console.log(url)
---
+++ src/background.js
@@ -0,0 +1,54 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'canvas.wpi.edu', pathPrefix: '/courses/' },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()],
+      },
+    ]);
+  });
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === 'DOWNLOAD_BUTTON_CLICKED') {
+    console.log(message);
+    downloadFile('https://canvas.wpi.edu/api/v1/files/6230398');
+  }
+});
+
+async function downloadFile(url) {
+  const response = await fetch(url);
+  const responseJson = await response.json();
+
+  chrome.downloads.download({
+    url: responseJson.url,
+    filename: responseJson.filename, //Optional
+  });
+}
+
+// if on https://<canvas>/courses/<course_id>/* (course specific download)
+// Download Modules, Files, (Assignments, Announcements, Discussions)
+// Modules:
+// List all modules: https://<canvas>/api/v1/courses/<course_id>/modules (call modules url, returns json)
+// For each module:
+//   let title = module["title"]
+//   let items_url = module["items_url"]
+//   List all module_items (call items_url, returns json)
+//     For each module_item:
+//       if module_item["type"] == "File"
+//         content_id = module_item["content_id"]
+//         let file_object = call https://<canvas>/api/v1/files/<content_id> (returns json)
+//         let file_name = file_object["filename"]
+//         let file_contents_blob = call file_object["url"] (returns binary data)
+
+// Files:
+// List all files: https://canvas.wpi.edu/api/v1/courses/<course_id>/files (call files url, returns json)
+// For each file object:
+//   let name = file["filename"]
+//   let file_contents_blob = call file["url"] (returns binary data)
+//
+// if on https://<canvas> or https://<canvas>/?login_success=1 (download from all courses)
